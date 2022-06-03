3ea9a82c4402eeb928af6c36a06ed4116c1580d4
YoonMinSoo <minsoo9615@naver.com>
2022-06-03 15:17:27 +0900

    #0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
-const express = require("express");
-
-// import express from "express";
+// const express = require("express");
+import express from "express";
 const app = express();
 
-app.listen(3000);
+app.set('view engine', "pug");
+app.set("views", __dirname + "/views");
+
+app.get("/",(req, res) => {
+    res.render("home");
+});
+app.use("/public", express.static(__dirname + "/public"));
+
+const handleListen = () => console.log("Listening on http://127.0.0.1:3000");
+app.listen(3000,handleListen);
