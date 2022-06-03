bd5ffdd5efef980db3819cb74922572a695d7dc4
YoonMinSoo <minsoo9615@naver.com>
2022-06-03 17:38:34 +0900

    #1.3 WebSocket Events

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ ___
 - ws 모듈을 통해 nodeJS에서 web-socket을 사용할 수 있다.
 - 하지만 프로젝트에서는 ws모듈에 채팅 기능이 구현된 응용 모듈을 사용할 것이다.
 - http모듈을 통해 http, ws 프로토콜을 사용하는 두 서버를 하나에 돌릴 수 있다.
+
+## #1.3 WebSocket Events
+- WS도 eventListener 사용방법과 유사하게 쓸 수 있다.
+- Listen가능한 Event들이 정해져있다.
--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/",(req, res) => {
     res.render("home");
 });
 app.get("/*",(req,res) => res.redirect("/"));
-app.use("/public", express.static(__dirname + "/public"));
 
 const handleListen = () => console.log("Listening on http://127.0.0.1:3000");
 // app.listen(3000,handleListen);
@@ -19,5 +19,8 @@ const handleListen = () => console.log("Listening on http://127.0.0.1:3000");
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({server});
+
+const handleConnection = (socket) => console.log(socket);
+wss.on("connection",handleConnection);
 
 server.listen(3000, handleListen);
